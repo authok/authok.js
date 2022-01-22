2b6d62bb1a9f9a668b69e0e2117702e4f6ced0b3
newtalentxp <newtalentxp@126.com>
2022-01-22 22:42:07 +0800

    fix

--- src/web-auth/index.js
+++ src/web-auth/index.js
@@ -671,6 +671,22 @@ WebAuth.prototype.changePasswordByEmail = function (options, cb) {
 };
 
 /**
+ * Request an sms with instruction to change a user's password
+ *
+ * @method changePasswordBySms
+ * @param {Object} options
+ * @param {String} options.phoneNumber phoneNumber where the user will receive the change password. It should match the user's email in authok
+ * @param {String} options.vcode address where the user will receive the change password email. It should match the user's email in authok
+ * @param {String} options.connection name of the connection where the user was created
+ * @param {changePasswordCallback} cb
+ * @see   {@link https://docs.authok.cn/api/authentication#change-password}
+ * @memberof WebAuth.prototype
+ */
+WebAuth.prototype.changePasswordBySms = function (options, cb) {
+  return this.client.dbConnection.changePasswordBySms(options, cb);
+};
+
+/**
  * Starts a passwordless authentication transaction.
  *
  * @method passwordlessStart
